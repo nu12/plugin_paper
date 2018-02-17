da86292f6bf5d0ca5a6b008eeac958f1c3593cdd
Alysson A Costa <alysson.avila.costa@gmail.com>
2018-02-17 20:23:09 +0000

    Appended migration

--- lib/plugin_paper/engine.rb
+++ lib/plugin_paper/engine.rb
@@ -1,4 +1,12 @@
 module PluginPaper
   class Engine < ::Rails::Engine
+
+  	initializer :append_migrations do |app|
+  	  unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+  	end
   end
 end
